79d75bd3775ad8ea4d3f1b0f6e4b6c28f50bb702
haamzzaa <hamzamr48@gmail.com>
2026-04-03 19:45:36 +0530

    Add DRF API endpoints

--- expenses/views.py
+++ expenses/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render, redirect
 from .models import Expense
 from .forms import ExpenseForm
 from django.db.models import Sum
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import ExpenseSerializer
 
 def index(request):
     expenses = Expense.objects.all()
@@ -22,3 +26,38 @@ def delete_expense(request, id):
     expense = Expense.objects.get(id=id)
     expense.delete()
     return redirect('index')
+@api_view(['GET', 'POST'])
+def expense_list(request):
+    if request.method == 'GET':
+        expenses = Expense.objects.all()
+        serializer = ExpenseSerializer(expenses, many=True)
+        return Response(serializer.data)
+
+    if request.method == 'POST':
+        serializer = ExpenseSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def expense_detail(request, id):
+    try:
+        expense = Expense.objects.get(id=id)
+    except Expense.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = ExpenseSerializer(expense)
+        return Response(serializer.data)
+
+    if request.method == 'PUT':
+        serializer = ExpenseSerializer(expense, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    if request.method == 'DELETE':
+        expense.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
