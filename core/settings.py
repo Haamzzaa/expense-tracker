357f440092c55470e42c5e0d0986b136b04becb0
haamzzaa <hamzamr48@gmail.com>
2026-04-04 12:44:37 +0530

    fix csrf and drf permissions

--- core/settings.py
+++ core/settings.py
@@ -12,7 +12,9 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ALLOWED_HOSTS = ['expense-tracker-o6gb.onrender.com', 'localhost', '127.0.0.1', '.railway.app']
-CSRF_TRUSTED_ORIGINS = ['https://*.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://*.railway.app'
+                        'https://expense-tracker-frontend-three-xi.vercel.app'
+                        ]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -84,3 +86,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "https://expense-tracker-frontend-three-xi.vercel.app",
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [],
+    'DEFAULT_PERMISSION_CLASSES': [],
+}
